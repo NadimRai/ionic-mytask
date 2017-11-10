5865d0be4cf9af5e0193b821cc88548f56749f19
NadimRai <rai_nadim@yahoo.com>
2017-11-11 00:33:45 +0800

    ability to display list of tasks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,31 @@ import { Task } from './task';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public tasks : Task [];
 
+  constructor(public navCtrl: NavController) {
+    this.tasks = [
+      {id: 1, title: 'Wash the dishes', isDone: false, isEdit: false},
+      {id: 2, title: 'Call mommy', isDone: false, isEdit: false},
+      {id: 3, title: 'Trains at Tutorial Dojo', isDone: false, isEdit: false},
+    ];
+  }
+
+  //Add Task 
+  addTask(title){
+    if(title){
+      this.tasks.push(new Task(title));
+    }
+  }
+
+  //Edit Task
+  editTask(task){
+
+  }
+
+  //Delete Task
+  deleteTask(id){
+    this.tasks.splice(id,1);
   }
 
 }
